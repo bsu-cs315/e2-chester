60e10428be7fa391621bc52f4885a020d892edb4
Ethan Chester <43710677+ethan-chester@users.noreply.github.com>
2024-09-23 23:54:14 -0400

    add gems to world

--- project/player.gd
+++ project/player.gd
@@ -2,24 +2,20 @@ extends CharacterBody2D
 
 
 const SPEED = 600.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -700.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
 	var direction := Input.get_axis("move_left", "move_right")
 	
 	if(direction > 0):
@@ -34,10 +30,6 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("walk")
 	else:
 		animated_sprite.play("jump")
-		
-		
-	
-		
 	
 	if direction:
 		velocity.x = direction * SPEED
---
+++ project/world.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _process(_delta: float) -> void:
+	
+	if (get_tree().get_nodes_in_group("gems").size()) <= 0:
+		%GemsLeft.text = "You Collected All The Gems!"
+		await get_tree().create_timer(4).timeout 
+		get_tree().change_scene_to_file("res://main_menu.tscn")
+	else:
+		%GemsLeft.text = "Gems Left To Collect: " + str(get_tree().get_nodes_in_group("gems").size())
