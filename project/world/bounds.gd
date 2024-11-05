ecfa0d59f0536f1fe4ac19d3f6d8049f4a18906c
Ethan Chester <43710677+ethan-chester@users.noreply.github.com>
2024-11-04 22:08:33 -0500

    Fix world bounds

--- project/world/bounds.gd
+++ project/world/bounds.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 
 func _on_area_entered(_area: Area2D) -> void:
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+	get_tree().change_scene_to_file("res://world/main_menu.tscn")
